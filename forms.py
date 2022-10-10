99b31dffeb997e84bb43de20975eac485e41be70
sam <samslusky@gmail.com>
2022-10-09 22:10:59 -0400

    Working on Forms
    created forms file based on
    
    https://www.digitalocean.com/community/tutorials/how-to-use-and-validate-web-forms-with-flask-wtf

---
+++ forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
+                     RadioField)
+from wtforms.validators import InputRequired, Length
