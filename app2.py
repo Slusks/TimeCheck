aac9eaff5b7f97329084f05857703bd504ab32b7
sam <samslusky@gmail.com>
2022-10-21 21:16:35 -0400

    Working on data formatting
    Working on how I want my data to be stored so I can then figure out how I want to display it.
    
    building functions to pass data back and forth
    
    writing to csv is e-z

--- app2.py
+++ app2.py
@@ -7,6 +7,7 @@ import os
 import datetime
 import calendar
 import csv
+from csv import DictWriter
 from pathlib import Path
 import pandas as pd
 
@@ -33,13 +34,21 @@ def home():
         'jobs':jobs
     }
     if request.method == "POST":
+        
         subject = request.form.get("CategoryInput")
-        day = request.form.get("DayInput")
+        dayWorked = request.form.get("DayInput")
         hour = request.form.get("HourInput")
         comment = request.form.get("commentInput")
-        time =datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+        timestamp =datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+        payload = {"subject": subject,
+                    "dayWorked": dayWorked,
+                    "hour": hour,
+                    "comment": comment,
+                    "timestamp": timestamp,
+                    }
+        update_csv(file2, payload)
         
-        print("output: ", subject, day, hour, comment, time)
+        #print("output: ", subject, dayWorked, hour, comment, timestamp)
     return render_template('index.html', data=data)
 
 @app.route('/engineers/')
@@ -51,14 +60,15 @@ def engineers():
                 "Ezekiel"]
     return render_template("engineers.html", engineers=engineers)
 
-@app.route('/person/')
-def person():
-    persons = ["Trevor",
-                "Bill",
-                "Nate",
-                "Sam",
-                "Ezekiel"]
-    return render_template("person.html", persons=persons)
+@app.route('/timekeeper/')
+def timekeeper():
+        timekeeperdata = {
+            'utc_dt':date_time_str,
+            'day':today,
+            'file':table,
+            'engineer':engineer,
+            'jobs':jobs}
+        return render_template("person.html", timekeeperdata=timekeeperdata)
 
 now = datetime.datetime.now()
 date_time_str = now.strftime("%m-%d-%Y %H:%M:%S")
@@ -68,17 +78,21 @@ today = calendar.day_name[now.weekday()]
 
 home_template = Path(r'C:\Users\sam\webdev\timecheck\template.csv')
 work_template = Path(r'X:\Sam Slusky\web\timeCheck\template.csv')
+home_template2 = Path(r'C:\Users\sam\webdev\timecheck\template2.csv')
+work_template2 = Path(r'X:\Sam Slusky\web\timeCheck\template2.csv')
 home_controller = Path(r'C:\Users\sam\webdev\timecheck\controller.csv') # column name: Categorys,Projects,Engineers,Team
 work_controller = Path(r'X:\Sam Slusky\web\timeCheck\controller.csv')
 table={}
-work = True
+work = False
 
 
 if work:
     file = work_template
+    file2 = work_template2
     controller = work_controller
 else:
     file = home_template
+    file2 = home_template2
     controller = home_controller
 
     
@@ -92,7 +106,7 @@ with open(file) as csv_file:
         else:
             table[str(line_count)]=row
             line_count +=1
-            continue
+            
 
 with open(controller) as csv_controller:
     df = pd.read_csv(csv_controller)
@@ -100,6 +114,28 @@ with open(controller) as csv_controller:
     engineer = [user, df[df['Engineers']==user].Team.item()]
     #print(engineer)
     jobs = {"category":df['Categorys'].tolist(), "projects":df['Projects'].tolist()}
+
+
+with open(file2) as csv_file2:
+    csv_reader2 = csv.reader(csv_file2, delimiter=',')
+    line_count = 0
+    for row in csv_reader2:
+        if line_count == 0:
+            table['headers'] = row
+            line_count +=1
+        else:
+            table[str(line_count)]=row
+            line_count +=1
+            
+
+def update_csv(filename, data):
+    print("updating CSV")
+    field_names = list(data.keys())
+    with open(filename, 'w', newline="") as file_object:
+        dictwriter_object= DictWriter(file_object, fieldnames=field_names)
+        dictwriter_object.writerow(data)
+        file_object.close()
+    print(data)
 
 
 if __name__=='__main__':
